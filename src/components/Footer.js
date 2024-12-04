080a869a3a9309d7e23b601fa1911fc1fbfc5662
unknown <yousef-1122@hotmail.com>
2024-12-04 03:01:25 +0300

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.css";
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer__container">
+                <p>© 2021. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
