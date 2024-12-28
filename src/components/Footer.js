58dd9594110fb78a4f0f8c2c60e561a6affc0e0f
unknown <yousef-1122@hotmail.com>
2024-12-28 10:10:51 +0300

    refactor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,7 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <footer className="footer">
-            <div className="footer__container">
-                <p>© 2024. All rights reserved.</p>
-            </div>
+            <p>© 2024. All rights reserved.</p>
         </footer>
     );
 };
